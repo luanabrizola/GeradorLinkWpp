3648791325aac8c33b913c3c72b59313d068c8ae
luanabrizola <130062674+luanabrizola@users.noreply.github.com>
2024-05-03 09:48:34 -0300

    Atualizações
    Começo de javaScript

---
+++ script.js
@@ -0,0 +1,9 @@
+
+const btnLink = document.getElementById('btn-link');
+const txtSend = document.getElementById('txt-send');
+
+
+
+btnLink.addEventListener('click', handleBtnLink);
+txtSend.addEventListener('click', handleTxtSend);
+
