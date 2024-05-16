25cdeece96c31f6f33f1440cc356584dce21b2e7
luanabrizola <130062674+luanabrizola@users.noreply.github.com>
2024-05-16 15:59:05 -0300

    Finalizado
    colocando a função de validar o numero e a mascara

--- script.js
+++ script.js
@@ -7,7 +7,8 @@ const numero = document.getElementById ('number')
 
 
 function handlebtnLink(){
-    let telefoneLink = 'https://wa.me/55'+numero.value
+    let telefone = numero.value.replace(/\D/g, '');
+    let telefoneLink = 'https://wa.me/55'+ telefone;
     link.innerText = telefoneLink
     mensagem.innerText = "Clique no link para copiar"
     link.style.border='1px solid black'
@@ -15,7 +16,8 @@ function handlebtnLink(){
 }
 
 function handletxtSend() {
-  let url = 'https://wa.me/55'+numero.value;
+  let telefone = numero.value.replace(/\D/g, '');
+  let url = 'https://wa.me/55'+ telefone;
   let win = window.open(url, '_blank');
   win.focus();
 }
@@ -25,6 +27,35 @@ function copiarLink(){
     .then(()=> mensagem.textContent = 'Link copiado com sucesso')
 }
 
+function mascara(telefone){
+  telefone = telefone.replace(/\D/g, '');
+  telefone = telefone.slice(0,11);
+  telefone = telefone.replace(/(\d{2})(\d{4,5})(\d{4})/, '($1) $2-$3')
+  return telefone;
+}
+
+function validarNumero(){
+  const numeroValido = numero.value.length === 15;
+  if (numeroValido){
+    numero.style.color = 'green'
+    btnLink.disabled = false;
+    txtSend.disabled = false
+  } else {
+    numero.style.color = 'black'
+    btnLink.disabled = true;
+    txtSend.disabled = true;
+  }
+}
+
+numero.addEventListener('input', ()=> {
+  numero.value = mascara(numero.value);
+  validarNumero()
+})
+
+
 btnLink.addEventListener('click', handlebtnLink);
 txtSend.addEventListener('click', handletxtSend);
 link.addEventListener('click', copiarLink)
+
+btnLink.disabled = true;
+txtSend.disabled = true;
